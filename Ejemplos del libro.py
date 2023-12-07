3ffb9555204214dffb0d251e2cdc940562d24038
jfNatanahel <natanahel.jf@gmail.com>
2023-12-07 00:20:28 -0300

    revisado

---
+++ Ejemplos del libro.py
@@ -0,0 +1,10 @@
+#Arreglo del 0 al 9 usando numpy
+import numpy as np 
+"""a=numpy.arange(10)
+print(a)"""
+
+#Crear un arreglo multidimensional 2x2 con tipo de dato int32
+A=np.array([[1,2],[2,4]],np.int32)
+print(A)
+print(A.shape) #Forma del arreglo
+print(A.size) #El tamaño
